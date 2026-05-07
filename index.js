ae901eab400f729182665c739cdd3100e6c1d61d
pedrenrique <pedrohfs.2207@gmail.com>
2026-05-07 09:27:12 -0300

    Testando as abas da tela de corrida

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('<h1>Servidor Runcity funcionando</h1>');
-});
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
